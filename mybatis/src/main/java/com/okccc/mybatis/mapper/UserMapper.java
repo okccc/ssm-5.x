332169516453be9d45fac1b050598e82580a751f
okccc <1573976179@qq.com>
2022-10-08 11:40:02 +0800

    idea设置mybatis核心配置文件和映射文件模板

--- mybatis/src/main/java/com/okccc/mybatis/mapper/UserMapper.java
+++ mybatis/src/main/java/com/okccc/mybatis/mapper/UserMapper.java
@@ -6,6 +6,8 @@ import com.okccc.mybatis.pojo.User;
  * Author: okccc
  * Date: 2022/9/28 4:03 下午
  * Desc: mapper接口
+ * idea设置mybatis核心配置文件和映射文件模板
+ * Intellij IDEA - Preferences - Editor - File and Code Templates - Files - mybatis-config/mybatis-mapper
  */
 public interface UserMapper {
 
