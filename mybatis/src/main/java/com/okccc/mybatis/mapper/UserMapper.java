f1a86cff7beafc5ce01efa43bc8ea6a26cdd151c
okccc <1573976179@qq.com>
2022-10-09 12:32:04 +0800

    mybatis给参数添加@Param("key")注解

--- mybatis/src/main/java/com/okccc/mybatis/mapper/UserMapper.java
+++ mybatis/src/main/java/com/okccc/mybatis/mapper/UserMapper.java
@@ -1,13 +1,19 @@
 package com.okccc.mybatis.mapper;
 
 import com.okccc.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Author: okccc
  * Date: 2022/9/28 4:03 下午
  * Desc: mapper接口
+ *
  * idea设置mybatis核心配置文件和映射文件模板
- * Intellij IDEA - Preferences - Editor - File and Code Templates - Files - mybatis-config/mybatis-mapper
+ * Intellij IDEA - Preferences - Editor - File and Code Templates - Files - mybatis-config/mapper
+ *
+ * mybatis参数类型
+ * 1.java实体类
+ * 2.单个或多个字段,通常会给参数添加@Param("key")注解,通过#{key}或者${key}获取值
  */
 public interface UserMapper {
 
@@ -17,4 +23,16 @@ public interface UserMapper {
     void insertUser(User user);
 
     void insertUserWithGeneratedKey(User user);
+
+    /**
+     * 修改用户
+     */
+    void updateUser(@Param("username") String username, @Param("password") String password);
+
+    /**
+     * 删除用户
+     */
+    void deleteUserById(@Param("id") Integer id);
+
+    void deleteUserByIds(@Param("ids") String ids);
 }
