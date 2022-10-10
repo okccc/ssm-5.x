1e8e0624ff46d3d81a741ee35060dddc3ec2e930
okccc <1573976179@qq.com>
2022-10-10 12:03:22 +0800

    mybatis获取参数值的两种方式#{}和${}

--- mybatis/src/main/java/com/okccc/mybatis/mapper/UserMapper.java
+++ mybatis/src/main/java/com/okccc/mybatis/mapper/UserMapper.java
@@ -1,7 +1,11 @@
 package com.okccc.mybatis.mapper;
 
 import com.okccc.mybatis.pojo.User;
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Author: okccc
@@ -14,6 +18,10 @@ import org.apache.ibatis.annotations.Param;
  * mybatis参数类型
  * 1.java实体类
  * 2.单个或多个字段,通常会给参数添加@Param("key")注解,通过#{key}或者${key}获取值
+ *
+ * mybatis获取参数值的两种方式
+ * 1.${}字符串拼接,可能存在sql注入问题
+ * 2.#{}占位符赋值(推荐),但是有些特殊sql只能使用字符串拼接
  */
 public interface UserMapper {
 
@@ -35,4 +43,49 @@ public interface UserMapper {
     void deleteUserById(@Param("id") Integer id);
 
     void deleteUserByIds(@Param("ids") String ids);
+
+    /**
+     * 根据id查询用户
+     */
+    User getUserById(@Param("id") Integer id);
+
+    /**
+     * 查询所有用户
+     */
+    List<User> getAllUser();
+
+    /**
+     * 登录校验
+     */
+    User checkLogin(@Param("username") String username, @Param("password") String password);
+
+    /**
+     * 统计行数
+     */
+    Integer getCount();
+
+    /**
+     * 将单个查询结果封装成Map集合,区别在于User类会返回所有字段而Map集合会过滤value=null的key
+     */
+    Map<String, Object> getUserByIdToMap(@Param("id") Integer id);
+
+    /**
+     * 将多个查询结果封装成Map集合,有两种方法
+     * 1.将mapper接口中方法的返回值设置为泛型为Map的List集合(常用)
+     * 2.将多个小Map放到一个大Map,由于Map放的是k-v对,所以要给小Map指定key,可以添加@MapKey注解将某个字段作为key
+     */
+    List<Map<String, Object>> getAllUserToMap01();
+
+    @MapKey("id")
+    Map<String, Object> getAllUserToMap02();
+
+    /**
+     * 模糊查询
+     */
+    List<User> getUserByLike(@Param("str") String str);
+
+    /**
+     * 动态传入表名,比如普通用户表和vip用户表字段都是一样的,就要根据传入的表名查询相应数据
+     */
+    List<User> selectByTableName(@Param("tableName") String tableName);
 }
--- mybatis/src/test/java/com/okccc/mybatis/test/MybatisTest.java
+++ mybatis/src/test/java/com/okccc/mybatis/test/MybatisTest.java
@@ -6,6 +6,9 @@ import com.okccc.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Author: okccc
  * Date: 2022/10/3 11:22 上午
@@ -14,23 +17,67 @@ import org.junit.Test;
 public class MybatisTest {
 
     @Test
-    public void testInsertUser() {
+    public void testInsert() {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        User user = new User(null, "moon", "ne", 20, "女", "ne@qq.com");
-        System.out.println(user);  // User{id=null, username='moon', ...}
-        userMapper.insertUser(user);
-        System.out.println(user);  // User{id=null, username='moon', ...}
-        sqlSession.close();
+        // 插入数据
+        User user01 = new User(null, "grubby", "orc", 19, "男", "orc@qq.com");
+        System.out.println(user01);  // User{id=null, username='grubby', ...}
+        userMapper.insertUser(user01);
+        System.out.println(user01);  // User{id=null, username='grubby', ...}
+        // 插入数据并获取自增主键
+        User user02 = new User(null, "moon", "ne", 19, "女", "ne@qq.com");
+        System.out.println(user02);  // User{id=null, username='moon', ...}
+        userMapper.insertUserWithGeneratedKey(user02);
+        System.out.println(user02);  // User{id=23, username='moon', ...}
     }
 
     @Test
-    public void testInsertUserWithGeneratedKey() {
+    public void testUpdate() {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        User user = new User(null, "moon", "ne", 20, "女", "ne@qq.com");
-        System.out.println(user);  // User{id=null, username='moon', ...}
-        userMapper.insertUserWithGeneratedKey(user);
-        System.out.println(user);  // User{id=11, username='moon', ...}
+        // 修改数据
+        userMapper.updateUser("moon", "123456");
+    }
+
+    @Test
+    public void testDelete() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        // 删除单条
+        userMapper.deleteUserById(19);
+        // 删除多条
+        userMapper.deleteUserByIds("8,9");
+    }
+
+    @Test
+    public void testSelect() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        // 根据id查询
+        User user01 = userMapper.getUserById(22);
+        System.out.println(user01);
+        // 查询所有
+        List<User> users = userMapper.getAllUser();
+        users.forEach(System.out::println);
+        // 登录校验
+        User user02 = userMapper.checkLogin("moon", "ne");
+        System.out.println(user02);
+        // 获取行数
+        Integer count = userMapper.getCount();
+        System.out.println(count);
+        // 将查询结果User实体类转换成Map集合
+        Map<String, Object> map01 = userMapper.getUserByIdToMap(21);
+        System.out.println(map01);
+        List<Map<String, Object>> list01 = userMapper.getAllUserToMap01();
+        System.out.println(list01);
+        Map<String, Object> map02 = userMapper.getAllUserToMap02();
+        System.out.println(map02);
+        // 模糊查询
+        List<User> list02 = userMapper.getUserByLike("g");
+        System.out.println(list02);
+        // 动态传入表名查询
+        List<User> list03 = userMapper.selectByTableName("t_user");
+        System.out.println(list03);
     }
 }
