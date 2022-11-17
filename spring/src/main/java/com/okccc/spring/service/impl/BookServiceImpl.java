1008dd30ccdf4366f2bc5a7172a5e8b73908da8f
okccc <1573976179@qq.com>
2022-11-18 07:15:44 +0800

    声明式事务属性之只读

--- spring/src/main/java/com/okccc/spring/service/impl/BookServiceImpl.java
+++ spring/src/main/java/com/okccc/spring/service/impl/BookServiceImpl.java
@@ -4,11 +4,14 @@ import com.okccc.spring.dao.BookDao;
 import com.okccc.spring.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Author: okccc
  * Date: 2022/11/7 6:04 下午
- * Desc:
+ * Desc: 事务属性之只读、超时、回滚策略、隔离级别、传播行为
  */
 @Service
 public class BookServiceImpl implements BookService {
@@ -17,7 +20,18 @@ public class BookServiceImpl implements BookService {
     private BookDao bookDao;
 
     @Override
+    @Transactional(
+            // 只读：表示当前操作不涉及写操作,这样数据库就能针对查询操作进行优化
+            // Connection is read-only. Queries leading to data modification are not allowed
+            readOnly = true
+    )
     public void buyBook(Integer userId, Integer bookId) {
+        try {
+            // 睡眠5秒验证事务超时
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         // 查询图书价格
         Integer price = bookDao.getPriceByBookId(bookId);
         // 更新图书库存
